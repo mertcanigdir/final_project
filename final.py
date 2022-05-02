77c77c1b9044636323b93e548c4b9ea426ef66f5
emirhanakbyk <emrhn.akbyk@gmail.com>
2022-05-02 21:49:47 +0300

    mesaj

--- final.py
+++ final.py
@@ -5,4 +5,4 @@ driver_path ="C:\Users\emrea\Desktop\final_project\chromedriver.exe"   #burada w
 browse =webdriver.Chrome(driver_path)
 browse.get("https://www.google.com.tr") #chrome üzerinden hangi siteye giriş yapmak istediğimiz parantez içine yazmamız yeterli 
 time.sleep(5)                   # chrome un ne kadar süre ile açık kalacağını 
-browse.quit()                  # ve süre bittikten sonra ne olacağını söylüyoruz 
+browse.quit()                  # ve süre bittikten sonra ne olacağını söylüyoruz. 
