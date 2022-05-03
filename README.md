c7de7ffeea5f15cdcfdacafff9fe51b487ce992d
MERTCANIĞDIR <mertcan.igdir@gmail.com>
2022-05-03 15:17:32 +0300

    mail gönderme

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 https://youtu.be/MIaIM6CLViQ   => resimden rakamları alındığı modül (easyocr)
 
 https://youtu.be/XvwPdJz4bJo => herhangi bir site üzerinden veri çekilmesini sağlayan modül (selenium)
+
+https://mertmekatronik.com/python-ile-mail-gondermek => mail gönderme modülü (smtplib)
+https://myaccount.google.com/lesssecureapps => Güvenliği düşük uygulama erişimi
--- final_projesi.py
+++ final_projesi.py
@@ -1,24 +1,43 @@
-from selenium import webdriver # burdaki import ile chrome üzerinden herhangi bir sayfaya giriş yapmamızı sağlıyor
-import time 
-
-driver_path ="C:\Users\emrea\Desktop\final_project\chromedriver.exe"   #burada web driverın bilgasayarımızda indirildiği yerin konumu
-browse =webdriver.Chrome(driver_path)
-browse.get("https://www.google.com.tr") #chrome üzerinden hangi siteye giriş yapmak istediğimiz parantez içine yazmamız yeterli 
-time.sleep(5)                   # chrome un ne kadar süre ile açık kalacağını 
-browse.quit()                  # ve süre bittikten sonra ne olacağını söylüyoruz. 
+# ################################################################ herhangi bir site üzerinden veri çekilme
 
 
-import easyocr                             # pip install easyocr yaıp indiriyoruz 
+# from selenium import webdriver # burdaki import ile chrome üzerinden herhangi bir sayfaya giriş yapmamızı sağlıyor
+# import time 
 
-reader = easyocr.Reader(['en','ch_tra'])        # burda çözümlenmek istenen resimlerdeki yazının hangi dilde olduğunun kısaltmları  (kısaltmlar sayfasında var ) 
-
-
-results = reader.readtext('test.jpg')            #jpg formatı olması gerekiyor 
-
-text = ''
-
-for result in results:
-    text += result[1] + ''
+# driver_path ="C:\Users\emrea\Desktop\final_project\chromedriver.exe"   #burada web driverın bilgasayarımızda indirildiği yerin konumu
+# browse =webdriver.Chrome(driver_path)
+# browse.get("https://www.google.com.tr") #chrome üzerinden hangi siteye giriş yapmak istediğimiz parantez içine yazmamız yeterli 
+# time.sleep(5)                   # chrome un ne kadar süre ile açık kalacağını 
+# browse.quit()                  # ve süre bittikten sonra ne olacağını söylüyoruz. 
 
 
-print( 'sonuc' + text )
+# ################################################################## resimden rakamları alma 
+
+
+# import easyocr                             # pip install easyocr yaıp indiriyoruz 
+
+# reader = easyocr.Reader(['en','ch_tra'])        # burda çözümlenmek istenen resimlerdeki yazının hangi dilde olduğunun kısaltmları  (kısaltmlar sayfasında var ) 
+
+
+# results = reader.readtext('test.jpg')            #jpg formatı olması gerekiyor 
+
+# text = ''
+
+# for result in results:
+#     text += result[1] + ''
+
+
+# print( 'sonuc' + text )
+
+
+#################################################################### mail gönderme
+
+
+import smtplib                                                    #Kütüphanemizi çağırıyoruz
+
+content = "merhaba"                                               #content adında mesajımızı oluşturuyoruz
+mail = smtplib.SMTP("smtp.gmail.com",587)                         #SMTP'nin gmail aderine 587. porttan ulaşıyoruz
+mail.ehlo()                                                       #ehlo fonksiyonu ile kullanılabilir hale getiriyoruz
+mail.starttls()                                                   #starttls fonksiyonu ile bağlantımızı gizli hale getiriyoruz
+mail.login("mertcan.igdir@gmail.com","*************")                            #login fonksiyonu ile herhangi bir mail adresine giriş yapıyoruz
+mail.sendmail("mertcan.igdir@gmail.com","alıcınınmail@gmail.com",content)      #sendmail fonksiyonu ile göndereni, alıcıyı ve gönderilen metni belirliyoruz
