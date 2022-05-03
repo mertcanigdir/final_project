a72272be07ac2e4eb0ff7b67c4fa40d3e9249f3e
MERTCANIĞDIR <mertcan.igdir@gmail.com>
2022-05-03 21:13:49 +0300

    kamera açma

--- final_projesi.py
+++ final_projesi.py
@@ -32,12 +32,32 @@
 
 #################################################################### mail gönderme
 
-def mail_gönder():
-    import smtplib                                                    #Kütüphanemizi çağırıyoruz
+# def mail_gönder():
+#     import smtplib                                                    #Kütüphanemizi çağırıyoruz
 
-    content = "merhaba"                                               #content adında mesajımızı oluşturuyoruz
-    mail = smtplib.SMTP("smtp.gmail.com",587)                         #SMTP'nin gmail aderine 587. porttan ulaşıyoruz
-    mail.ehlo()                                                       #ehlo fonksiyonu ile kullanılabilir hale getiriyoruz
-    mail.starttls()                                                   #starttls fonksiyonu ile bağlantımızı gizli hale getiriyoruz
-    mail.login("mertcan.igdir@gmail.com","*************")                            #login fonksiyonu ile herhangi bir mail adresine giriş yapıyoruz
-    mail.sendmail("mertcan.igdir@gmail.com","alıcınınmail@gmail.com",content)      #sendmail fonksiyonu ile göndereni, alıcıyı ve gönderilen metni belirliyoruz
+#     content = "merhaba"                                               #content adında mesajımızı oluşturuyoruz
+#     mail = smtplib.SMTP("smtp.gmail.com",587)                         #SMTP'nin gmail aderine 587. porttan ulaşıyoruz
+#     mail.ehlo()                                                       #ehlo fonksiyonu ile kullanılabilir hale getiriyoruz
+#     mail.starttls()                                                   #starttls fonksiyonu ile bağlantımızı gizli hale getiriyoruz
+#     mail.login("mertcan.igdir@gmail.com","*************")                            #login fonksiyonu ile herhangi bir mail adresine giriş yapıyoruz
+#     mail.sendmail("mertcan.igdir@gmail.com","alıcınınmail@gmail.com",content)      #sendmail fonksiyonu ile göndereni, alıcıyı ve gönderilen metni belirliyoruz
+
+
+##################################################################### kamera açma
+
+
+import cv2
+from cv2 import VideoCapture
+from cv2 import waitKey
+ 
+kamera = cv2.VideoCapture(0)
+ 
+while (True):
+    ret, videoGoruntu = kamera.read()
+    cv2.imshow("Bilgisayar Kamerasi", videoGoruntu)
+    if cv2.waitKey(50) & 0xFF == ord('x'):
+        break
+ 
+kamera.release()
+cv2.destroyAllWindows()
+
