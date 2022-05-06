658f71d3fd2e049c9d9320d5e7e291837bdeb829
emrraras <95229165+emrraras@users.noreply.github.com>
2022-05-07 00:25:31 +0300

    easyocr den tesseract a geçildi

--- final_projesi.py
+++ final_projesi.py
@@ -1,39 +1,53 @@
-# # ################################################################ herhangi bir site üzerinden veri çekilme
+######################## herhangi bir site üzerinden veri çekilme
 
-from lib2to3.pgen2 import driver
-from selenium import webdriver                                             # burdaki import ile chrome üzerinden herhangi bir sayfaya giriş yapmamızı sağlıyor
-import time 
+# from lib2to3.pgen2 import driver
+# from selenium import webdriver                                             # burdaki import ile chrome üzerinden herhangi bir sayfaya giriş yapmamızı sağlıyor
+# import time 
 
-driver_path ="C:\\Users\\emrea\\Downloads\\chromedriver.exe"             # chrome driver exe sinin konumu 
-browser =webdriver.Chrome(driver_path)
-browser.get("https://www.epdk.gov.tr/Detay/Icerik/3-1327/elektrik-faturalarina-esas-tarife-tablolari") #chrome üzerinden hangi siteye giriş yapmak istediğimiz parantez içine yazmamız yeterli 
+# driver_path ="C:\\Users\\emrea\\Downloads\\chromedriver.exe"             # chrome driver exe sinin konumu 
+# browser =webdriver.Chrome(driver_path)
+# browser.get("https://www.epdk.gov.tr/Detay/Icerik/3-1327/elektrik-faturalarina-esas-tarife-tablolari") #chrome üzerinden hangi siteye giriş yapmak istediğimiz parantez içine yazmamız yeterli 
 
-button = browser.find_element_by_xpath('...........')  # tıklama yeri bulunacak 
-button.click()
-time.sleep(0)                   # chrome un ne kadar süre ile açık kalacağını 
-browser.quit()                  # ve süre bittikten sonra ne olacağını söylüyoruz. 
+# button = browser.find_element_by_xpath('...........')  # tıklama yeri bulunacak 
+# button.click()
+# time.sleep(0)                   # chrome un ne kadar süre ile açık kalacağını 
+# browser.quit()                  # ve süre bittikten sonra ne olacağını söylüyoruz. 
 
 
 # ################################################################## resimden rakamları alma 
 
+import cv2
+import pytesseract
+from PIL import Image
 
-# import easyocr                             # pip install easyocr yaıp indiriyoruz 
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\emrea\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'    # tesseract.exe nin bilgisayarda kurulu olduğu yer
+def ocr_core(img):
+    text = pytesseract.image_to_string(img)
+    return text
 
-# reader = easyocr.Reader(['en','ch_tra'])        # burda çözümlenmek istenen resimlerdeki yazının hangi dilde olduğunun kısaltmları  (kısaltmlar sayfasında var ) 
+img =cv2.imread('C:\\Users\\emrea\Desktop\\New folder\\final_project\\img.png')  # okunacak resimin konumu
 
+def get_grayscale(image):
+    return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-# results = reader.readtext('test.jpg')            #jpg formatı olması gerekiyor 
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
 
-# text = ''
+def thresholding(image):
+    return cv2.threshold(image,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# for result in results:
-#      text += result[1] + ''
+img = get_grayscale(img)
+img= thresholding(img)
+img=remove_noise(img)
 
-
-#print( 'sonuc' + text )
+print(ocr_core(img))
+print(type(ocr_core(img)))
 
 
 #################################################################### mail gönderme
+
+
+
 
 # import smtplib                                                    #Kütüphanemizi çağırıyoruz
 
