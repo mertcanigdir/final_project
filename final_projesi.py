5e866ff9fc5c305dd41a099eb72b726d37b84204
emrraras <95229165+emrraras@users.noreply.github.com>
2022-05-10 17:24:01 +0300

    buskinin web sitesinden güncel fiyat çekildi

--- final_projesi.py
+++ final_projesi.py
@@ -1,47 +1,59 @@
-######################## herhangi bir site üzerinden veri çekilme
-
-# from lib2to3.pgen2 import driver
-# from selenium import webdriver                                             # burdaki import ile chrome üzerinden herhangi bir sayfaya giriş yapmamızı sağlıyor
-# import time 
-
-# driver_path ="C:\\Users\\emrea\\Downloads\\chromedriver.exe"             # chrome driver exe sinin konumu 
-# browser =webdriver.Chrome(driver_path)
-# browser.get("https://www.epdk.gov.tr/Detay/Icerik/3-1327/elektrik-faturalarina-esas-tarife-tablolari") #chrome üzerinden hangi siteye giriş yapmak istediğimiz parantez içine yazmamız yeterli 
-
-# button = browser.find_element_by_xpath('...........')  # tıklama yeri bulunacak 
-# button.click()
-# time.sleep(0)                   # chrome un ne kadar süre ile açık kalacağını 
-# browser.quit()                  # ve süre bittikten sonra ne olacağını söylüyoruz. 
-
-
-# ################################################################## resimden rakamları alma 
-
+from gettext import gettext
+from html.parser import HTMLParser
 import cv2
 import pytesseract
 from PIL import Image
+from lib2to3.pgen2 import driver
+from selenium import webdriver                                             # burdaki import ile chrome üzerinden herhangi bir sayfaya giriş yapmamızı sağlıyor
+import time 
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from bs4 import BeautifulSoup
+######################## herhangi bir site üzerinden veri çekilme
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\emrea\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'    # tesseract.exe nin bilgisayarda kurulu olduğu yer
-def ocr_core(img):
-    text = pytesseract.image_to_string(img)
-    return text
 
-img =cv2.imread('C:\\Users\\emrea\Desktop\\New folder\\final_project\\img.png')  # okunacak resimin konumu
 
-def get_grayscale(image):
-    return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+driver_path ="C:\\Users\\emrea\\Downloads\\chromedriver.exe"             # chrome driver exe sinin konumu 
+browser =webdriver.Chrome(driver_path)
+browser.get("https://www.buski.gov.tr/AboneRehberi/AboneRehberi/7") #chrome üzerinden hangi siteye giriş yapmak istediğimiz parantez içine yazmamız yeterli 
+browser.maximize_window()
+button = browser.find_element_by_xpath ('//*[@id="abone-rehberi-header-2"]/button')
+button.click()
+url = "https://www.buski.gov.tr/AboneRehberi/AboneRehberi/7"
+#requests.get("https://www.buski.gov.tr/AboneRehberi/AboneRehberi/7", verift = False)
+soup = BeautifulSoup(browser.page_source,'html.parser')
+bilgi=[td.text for td in soup.findAll('td',style="text-align:center")]
+table =soup.find('div',attrs = {'id':'quotesList'})
+time.sleep(10)                   # chrome un ne kadar süre ile açık kalacağını 
+browser.quit()                  # ve süre bittikten sonra ne olacağını söylüyoruz. 
+print(bilgi[0])
 
-def remove_noise(image):
-    return cv2.medianBlur(image,5)
+# ################################################################## resimden rakamları alma 
 
-def thresholding(image):
-    return cv2.threshold(image,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-img = get_grayscale(img)
-img= thresholding(img)
-img=remove_noise(img)
+# pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\emrea\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'    # tesseract.exe nin bilgisayarda kurulu olduğu yer
+# def ocr_core(img):
+#     text = pytesseract.image_to_string(img)
+#     return text
 
-print(ocr_core(img))
-print(type(ocr_core(img)))
+# img =cv2.imread('C:\\Users\\emrea\Desktop\\New folder\\final_project\\img.png')  # okunacak resimin konumu
+
+# def get_grayscale(image):
+#     return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+# def remove_noise(image):
+#     return cv2.medianBlur(image,5)
+
+# def thresholding(image):
+#     return cv2.threshold(image,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+# img = get_grayscale(img)
+# img= thresholding(img)
+# img=remove_noise(img)
+
+# print(ocr_core(img))
+# print(type(ocr_core(img)))
 
 
 #################################################################### mail gönderme
