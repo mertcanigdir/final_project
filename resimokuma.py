bc7cf78e499ad54ec25cd9a5461e30ed00dcad80
emirhanakbyk <emrhn.akbyk@gmail.com>
2022-05-24 00:54:25 +0300

    resim okuma

--- resimokuma.py
+++ resimokuma.py
@@ -14,15 +14,24 @@
 #cv2.imshow("resim", a)
 #cv2.waitKey(0)
 
-import pytesseract
-import cv2
+#import pytesseract
+#import cv2
 
-yol = "C:\\Users\\emrhn\\Documents\\GitHub\\final_project\\test.jpg"
+#yol = "C:\\Users\\emrhn\\Documents\\GitHub\\final_project\\test.jpg"
+
+#pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\emrhn\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+
+#resim = cv2.imread(yol)
+
+#metin = pytesseract.image_to_string(resim)
+
+#print(metin)
+
+from PIL import Image
+import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\emrhn\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
 
-resim = cv2.imread(yol)
+a=pytesseract.image_to_string(Image.open("susayaci.jpg"), lang="eng")
 
-metin = pytesseract.image_to_string(resim)
-
-print(metin)
+print(a)
