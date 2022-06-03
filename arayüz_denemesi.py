b42ba1593bb9982aac1ddc3232a04086a00b1740
emrraras <95229165+emrraras@users.noreply.github.com>
2022-06-03 12:22:59 +0300

    su tutarı ve vergi miktarı ayarlandı

--- arayüz_denemesi.py
+++ arayüz_denemesi.py
@@ -7,6 +7,12 @@ from setuptools import Command
 pencere = tk.Tk()
 pencere.geometry("700x500")
 
+sayı1 =tk.Entry(width=12)
+sayı1.place(x=200,y=10)
+
+sayı2 =tk.Entry(width=12)
+sayı2.place(x=200,y=30)
+
 
 
 def kdv():
@@ -18,8 +24,13 @@ def kdv():
 def kullanılan_su():
     s1=int(sayı1.get())
     s2=int(sayı2.get())
-    kullanılan["text"] = int((s2-s1)*12)
-
+    oniki_altı=89.4
+    oniki_ustu=((s2-s1)-12)*14.16
+    if s2-s1<=12 and s2-s1>=0:
+        kullanılan["text"] =(s2-s1)*7.45
+    elif s2-s1>12:
+         kullanılan["text"] =float(oniki_altı)+float(oniki_ustu)
+       
 
 def bakım():
     s1=int(sayı1.get())
@@ -30,14 +41,17 @@ def bakım():
 def atık():
     s1=int(sayı1.get())
     s2=int(sayı2.get())
-    if s2-s1>=48:
-        atık_bedeli["text"] = int((s2-s1)*(8))
+    oniki_altı_atık=1.86*12
+    oniki_ustu_atık=((s2-s1)-12)*3.54
+    if s2-s1<=12 and s2-s1>=0:
+        kullanılan["text"] =(s2-s1)*1.86
 
-    elif s2-s1 >0 and  s2-s1 <48:
-        atık_bedeli["text"] =int((s2-s1)*(2))
+        
+    elif s2-s1>12:
+         kullanılan["text"] =float(oniki_altı_atık)+float(oniki_ustu_atık)
 
     else:
-        print("yanlıs bir değr girdiniz")
+        print("yanlıs bir değer girdiniz")
 
 bilgi1=tk.Label(text="Başlagıç değerini giriniz :")
 bilgi1.place(x=20,y=10)
@@ -65,6 +79,8 @@ bakım_bedeli.place(x=200,y=190)
 
 atık_bedeli=tk.Label(text="=")
 atık_bedeli.place(x=200,y=230)
+
+
 
 hesap =tk.Button(text="Hesapla",width=15,command=lambda:[kullanılan_su(),kdv(),bakım(),atık()])
 hesap.place(x=300,y=15)
