79b750bba096445072d9696f1902e36a08b291b4
emrraras <95229165+emrraras@users.noreply.github.com>
2022-06-12 12:32:00 +0300

    düzeltme

--- arayüz_denemesi.py
+++ arayüz_denemesi.py
@@ -21,7 +21,7 @@ def kullanılan_su():
     s2=int(sayı2.get())
     x=s2-s1
     if(x>=0 and x<48):
-        kullanılan=x*1,75
+        kullanılan=x*1.75
     elif(s2-s1>=48):
         kullanılan=((x-48)*3.30)+(48*1.75)
 
